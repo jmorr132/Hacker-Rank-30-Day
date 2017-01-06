0eca168cababf210e6ca4f5a7ed0feb9f852620e
Jason Morrissette <j.morrissette@jamdevs.com>
2017-01-06 10:41:54 -0800

    FEAT: Completed Day 1 Challenge
    Task
    Complete the code in the editor below. The variables , , and  are
    already declared and initialized for you. You must:
    
    Declare  variables: one of type int, one of type double, and one of type
    String.
    Read  lines of input from stdin (according to the sequence given in the
    Input Format section below) and initialize your  variables.
    Use the  operator to perform the following operations:
    Print the sum of  plus your int variable on a new line.
    Print the sum of  plus your double variable to a scale of one decimal
    place on a new line.
    Concatenate  with the string you read as input and print the result on a
    new line.

---
+++ Day1.py
@@ -0,0 +1,16 @@
+j = int(raw_input())
+k = float(raw_input())
+y = raw_input()
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+ji = j + i
+dk = d + k
+sy = s + y
+# Print the sum of both integer variables on a new line.
+print ji
+# Print the sum of the double variables on a new line.
+print dk
+# Concatenate and print the String variables on a new line
+print sy
+# The 's' variable above should be printed first.
